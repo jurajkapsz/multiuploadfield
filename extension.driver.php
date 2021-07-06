b11906c2d397d63b92e6ed23dbc8e1f3cfc90dcb
Juraj Kapsz <dev@jurajkapsz.sk>
2021-07-06 09:30:42 +0200

    Fix SVG file uploads

--- extension.driver.php
+++ extension.driver.php
@@ -17,6 +17,28 @@ class extension_multiuploadfield extends Extension {
 
     public function update($previousVersion = null)
     {
+        // Prior version 1.6.3:
+        // Auto-update meta data of existing SVG entries
+        if (version_compare($previousVersion, '1.6.3', '<')) {
+            $multiupload_fields = FieldManager::fetch(null, null, 'ASC', 'id', 'multiupload');
+
+            foreach ($multiupload_fields as $field) {
+                $table_id = $field->get('id');
+                $file_path = DOCROOT . $field->get('destination');
+                $svg_files = Symphony::Database()->fetch("SELECT `id`, `file`, `mimetype` FROM `tbl_entries_data_$table_id` WHERE `mimetype` LIKE '%image/svg%'");
+
+                if (!empty($svg_files)) {
+                    foreach ($svg_files as $svg) {
+                        $entry_id = intval($svg['id']);
+                        $file = $file_path . '/' . $svg['file'];
+                        $mimetype = $svg['mimetype'];
+                        $meta = serialize(FieldMultiUpload::getMetaInfo($file, $mimetype));
+
+                        Symphony::Database()->query("UPDATE `tbl_entries_data_$table_id` SET `meta` = '" . $meta . "' WHERE `id` = $entry_id");
+                    }
+                }
+            }
+        }
     }
 
     public function uninstall()
