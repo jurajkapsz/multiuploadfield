60d9e84f4abff7e20d6cd2454bdf8554b30c3a4c
Juraj Kapsz <dev@jurajkapsz.sk>
2021-07-06 11:41:00 +0200

    Add Slovak translation

---
+++ lang/lang.sk.php
@@ -0,0 +1,42 @@
+<?php
+
+	$about = array(
+		'name' => 'Slovensky',
+		'author' => array(
+			'name' => 'Juraj Kapsz',
+			'email' => 'hi@jurajkapsz.sk',
+			'website' => 'https://jurajkapsz.sk'
+		),
+		'release-date' => '2021-07-06'
+	);
+
+	/**
+	 * Multi Upload Field
+	 */
+	$dictionary = array(
+
+		'Drop files' => 
+		'Pridajte súbory',
+
+		'In queue' => 
+		'Vo fronte',
+
+		'Uploading' => 
+		'Nahrávam',
+
+		'Remove file' => 
+		'Odstrániť',
+
+		'Upload failed' => 
+		'Nahrávanie zlyhalo',
+
+		'Multi File Upload' => 
+		false,
+
+		'Add file' => 
+		'Pridať súbor',
+
+		'The file, %s, is no longer available. Please check that it exists, and is readable.' => 
+		'Súbor %s nie je dostupný. Prosím, skontrolujte či súbor existuje a má práva na čítanie.',
+
+	);
